6308b36696a9a5f6d5c0b738cc5d0a387d7fd799
keerthana <keerthana.kasthuril@thoughtworks.com>
2021-01-08 23:35:42 +0530

    [KK] Implemented tail function

---
+++ src/tail.js
@@ -0,0 +1,8 @@
+const tail = (list) => {
+      var result = [];
+      for(var i=1; i<list.length; ++i){
+	    result.push(list[i]);
+      }
+      return result;
+}
+module.exports = tail;
---
+++ src/tail.spec.js
@@ -0,0 +1,13 @@
+const tail = require('./tail');
+
+describe('Tail', () => {
+
+     it('Tail of [] should be []', () => {
+          expect(tail([])).toEqual([]);
+     });
+
+     it('Tail of [1,2,3] is [2,3]', () => {
+          expect(tail([1,2,3])).toEqual([2,3]);
+     });
+
+})
