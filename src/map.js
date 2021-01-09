b32f6229f458e472f2da95a4c023fefacf40f9f8
keerthana <keerthana.kasthuril@thoughtworks.com>
2021-01-09 19:32:30 +0530

    [KK] Implemented generic map function

--- src/map.js
+++ src/map.js
@@ -1,13 +1,9 @@
 const map = (list, applyFunction) =>{ //List will be mapped to another list according to applyFunction
 	var functionAppliedList = [];
-	if (list.length == 0) {
-		return functionAppliedList;
+	for ( let i = 0; i < list.length; i++){
+		var functionAppliedElement = applyFunction(list[i], i, list.values());
+		functionAppliedList.push(functionAppliedElement);
 	}
-	else {
-		for ( var i = 0; i < list.length; i++){
-			var functionAppliedElement = applyFunction(list[i]);
-			functionAppliedList.push(functionAppliedElement);
-	}}
 	return functionAppliedList;
 }
 module.exports = map;
--- src/map.spec.js
+++ src/map.spec.js
@@ -21,6 +21,7 @@ describe('map', () => {
 		expect(map([1,2,3], identity)).toEqual([1,2,3]);
 	});
 	it('Testing explicit increment for an object', () => {
-		expect(map({x : 10}, someObject => someObject.x + 1 )).toEqual(11);
+		expect(map([{x : 10}], someObject=>someObject.x+1)).toEqual([11]);
 	});
+
 })
