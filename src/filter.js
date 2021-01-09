68bf78ee3b94b0dc66164d6f6c6e74dd50484b26
keerthana <keerthana.kasthuril@thoughtworks.com>
2021-01-09 20:39:59 +0530

    [KK] Corrected spacings and removed unwanted function call

--- src/filter.js
+++ src/filter.js
@@ -1,7 +1,7 @@
 const filter = (list, applyFunction) =>{ //List will be filtered according to applyFunction
 	var functionAppliedList = [];
 	for ( let i = 0; i < list.length; i++){
-		var filterStatus = applyFunction(list[i], i, list.values());
+		var filterStatus = applyFunction(list[i], i, list;
 		if (filterStatus){
 			functionAppliedList.push(list[i]);
 		}
--- src/map.js
+++ src/map.js
@@ -1,7 +1,7 @@
 const map = (list, applyFunction) =>{ //List will be mapped to another list according to applyFunction
 	var functionAppliedList = [];
 	for ( let i = 0; i < list.length; i++){
-		var functionAppliedElement = applyFunction(list[i], i, list.values());
+		var functionAppliedElement = applyFunction(list[i], i, list);
 		functionAppliedList.push(functionAppliedElement);
 	}
 	return functionAppliedList;
